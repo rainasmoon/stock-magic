f9939fb024485f1179390810d42b642471931fc2
rain <rainasmoon@126.com>
2020-11-06 14:14:55 +0800

    svm set gamma = scale. other than 0.1 & auto

--- thirdpart/ModelUtils.py
+++ thirdpart/ModelUtils.py
@@ -1,18 +1,21 @@
 from sklearn import svm
 import DC
+import Utils
 
 def use_svm(train, target, test_case):
-    model = svm.SVC(gamma='auto')               # 建模
+    model = svm.SVC(gamma = 'scale')# 建模
     model.fit(train, target)        # 训练
     ans2 = model.predict(test_case) # 预测
     return float(ans2[0])
 
 if __name__ == '__main__':
-    stock = '002049.SZ'
+    stock = Utils.stock_pool[0]
     dc = DC.data_collect(stock, '2017-03-01', '2018-03-01')
     train = dc.data_train
     target = dc.data_target
     test_case = [dc.test_case]
+    print(train)
+    print(test_case)
     aresult = use_svm(train, target, test_case)
     # 输出对2018-03-02的涨跌预测，1表示涨，0表示不涨。
     print(str(aresult))
